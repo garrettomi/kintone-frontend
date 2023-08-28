b09963828f650878654880b57e953cda5490f6d1
garrettomi <omigarrett@gmail.com>
2023-08-28 11:16:04 +0900

    Feat: Implement routing from clicking submit login to home

--- kintone-frontend/src/pages/Login/Login.js
+++ kintone-frontend/src/pages/Login/Login.js
@@ -1,10 +1,35 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (event) => {
+  const navigate = useNavigate();
+  const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // const response = await fetch(
+    //   `https://${process.env.SUBDOMAIN}.kintone.com/k/v1/record.json`,
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "X-Cybozu-API-Token": process.env.APITOKEN,
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       app: process.env.APPID,
+    //       record: {
+    //         value: username,
+    //       },
+    //       password: {
+    //         value: password,
+    //       },
+    //     }),
+    //   }
+    // );
+    // const data = await response.json();
+    // console.log(data);
+    navigate("/");
   };
   return (
     <form onSubmit={handleSubmit}>
