97737abcd30905508f210589c42626d40bd86d60
garrettomi <omigarrett@gmail.com>
2023-08-28 16:24:50 +0900

    Feat: Implement signup routing

--- kintone-frontend/src/pages/SignUp/SignUp.js
+++ kintone-frontend/src/pages/SignUp/SignUp.js
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../authentication/firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSignUp = async () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
+      navigate("/");
     } catch (error) {
       console.error(error);
     }
