fd29c58b54c8841639816d01c23882a8fb9efd39
garrettomi <omigarrett@gmail.com>
2023-08-29 08:48:29 +0900

    Feat: Update routes to make user default to Login page

--- kintone-frontend/src/App.js
+++ kintone-frontend/src/App.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import SignUp from "./pages/SignUp/SignUp";
@@ -7,13 +12,17 @@ import UserContext from "./context/UserContext";
 
 function App() {
   const [userEmail, setUserEmail] = useState(null);
+  const isLoggedIn = userEmail !== null;
   return (
     <UserContext.Provider value={{ userEmail, setUserEmail }}>
       <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
+          />
         </Routes>
       </Router>
     </UserContext.Provider>
