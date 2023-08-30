b3b6133f5561c2be90b823b2ade31030378bed51
garrettomi <omigarrett@gmail.com>
2023-08-30 17:13:11 +0900

    Fix: Delete unused code in Markers

--- kintone-frontend/src/components/Markers/Markers.js
+++ kintone-frontend/src/components/Markers/Markers.js
@@ -2,73 +2,14 @@ import { Marker, Popup } from "react-map-gl";
 import { useState } from "react";
 import "./Markers.css";
 
-const Markers = ({ latitude, longitude, offsetLeft, offsetTop, location }) => {
-  // const [showPopup, setShowPopup] = useState(false);
+const Markers = ({ location }) => {
   const [selectedLocation, setSelectedLocation] = useState(null);
-
-  // const togglePopup = () => {
-  //   setShowPopup(!showPopup);
-  // };
 
   const togglePopup = (loc) => {
     setSelectedLocation(selectedLocation === loc ? null : loc);
   };
 
-  console.log("LOCATION", location);
-
-  // const coordinates = location.map((loc) => ({
-  //   longitude: Number(loc.locCoordsX),
-  //   latitude: Number(loc.locCoordsY),
-  // }));
-
-  // const handleMarkerClick = (location) => {
-  //   setSelectedLocation(location);
-  // };
-
-  // console.log("COORDINATES", coordinates);
-
-  // const locationInfo = location.find(
-  //   (loc) => loc.coordinates[0] === longitude && loc.coordinates[1] === latitude
-  // );
-
-  // console.log("LOCATION INFO", locationInfo);
-
   return (
-    // <div>
-    //   <Marker latitude={latitude} longitude={longitude}>
-    //     <div
-    //       className="marker"
-    //       style={{ marginLeft: offsetLeft, marginTop: offsetTop }}
-    //       onClick={togglePopup}
-    //     >
-    //       📍
-    //     </div>
-    //   </Marker>
-    //   {showPopup && (
-    //     <Popup
-    //       latitude={latitude}
-    //       longitude={longitude}
-    //       closeButton={true}
-    //       closeOnClick={false}
-    //       onClose={togglePopup}
-    //       anchor="top"
-    //     >
-    //       <div>
-    //         <p>Country: {locationInfo.country}</p>
-    //         <p>State: {locationInfo.state}</p>
-    //         <p>City: {locationInfo.city}</p>
-
-    //         {locationInfo.imageUrl && (
-    //           <img
-    //             src={locationInfo.imageUrl}
-    //             className="locationImage"
-    //             alt="location"
-    //           />
-    //         )}
-    //       </div>
-    //     </Popup>
-    //   )}
-    // </div>
     <div>
       {location.map((loc, index) => (
         <Marker
