b0f4e6cde81817c1cd1fcd7105d9f0c2b6c116b4
garrettomi <omigarrett@gmail.com>
2023-08-31 06:27:59 +0900

    Fix: Prevent user from logging out after refreshing with onAuthStateChanged

--- kintone-frontend/src/App.js
+++ kintone-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
@@ -9,10 +9,31 @@ import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import SignUp from "./pages/SignUp/SignUp";
 import UserContext from "./context/UserContext";
+import auth from "./authentication/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const [userEmail, setUserEmail] = useState(null);
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserEmail(user.email);
+      } else {
+        setUserEmail(null);
+      }
+      setAuthReady(true);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  if (!authReady) {
+    return <div>Loading...</div>;
+  }
+
   const isLoggedIn = userEmail !== null;
+
   return (
     <UserContext.Provider value={{ userEmail, setUserEmail }}>
       <Router>
